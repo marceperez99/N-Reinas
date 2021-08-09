94b032626432098f2da986329c05292072c871f9
jbust97 <hugofleitasbus@gmail.com>
2021-08-09 10:19:34 -0400

    Modulariza el codigo

--- algoritmos/heuristica.js
+++ algoritmos/heuristica.js
@@ -1,123 +1,123 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-function mostrar(reinas) {
-    console.log(reinas)
-}
 
-const algoritmo_heuristico = (graficar_reina,n) => {
-    //Inicio del algoritmo
-    let reinas = new Array(n)
-    //Inicializa todas las variables aleatoriamente
+function inicializarTableroAleatoriamente(reinas){
+    let n = reinas.length
     for (let i = 0; i < n; i++) {
         reinas[i] = getRandomInt(n)
-
     }
-    //Proceso de reparación heurística
-    let paso = 0
-    let ultima_columna = -1
-    while (true) {
-        paso++
-
-        //Consigue una variable aleatoria que no satisfaga la condición del PSR
-        let reinas_en_conflicto = new Array(0)
-        let columnas_agregadas = new Map()
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                if (i != j) {
-                    if (reinas[i] == reinas[j] || i + reinas[i] == j + reinas[j] || (i - reinas[i]) == (j - reinas[j])) {
-                        if (!columnas_agregadas.has(i)) {
-                            reinas_en_conflicto.push(i)
-                            columnas_agregadas.set(i, true)
-                        }
+}
+function getVariablesEnConflicto(reinas){
+    let n = reinas.length
+    let reinasEnConflicto = new Array(0)
+    let columnasAgregadas = new Map()
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i != j) {
+                if (reinas[i] == reinas[j] || i + reinas[i] == j + reinas[j] || (i - reinas[i]) == (j - reinas[j])) {
+                    if (!columnasAgregadas.has(i)) {
+                        reinasEnConflicto.push(i)
+                        columnasAgregadas.set(i, true)
                     }
                 }
             }
         }
-        console.log("Paso: " + paso)
-        console.log("Estas son las columnas en conflicto")
-        console.log(reinas_en_conflicto)
+    }
+    return reinasEnConflicto
+}
+
+function getColumnaEnConflicto(reinasEnConflicto,ultimaColumna){
+    let columnaEnConflicto = reinasEnConflicto[getRandomInt(reinasEnConflicto.length)]
+    while (columnaEnConflicto == ultimaColumna)
+        columnaEnConflicto = reinasEnConflicto[getRandomInt(reinasEnConflicto.length)]
+    return columnaEnConflicto
+}
+function getConflictosEnColumna(reinas,columnaEnConflicto){
+    //Encontrar numero de conflictos
+    let n = reinas.length
+    let conflictos = new Array(n)
+    for(let i=0;i<n;i++){
+        conflictos[i]=0
+    }
+    //La i representa la fila
+    for (let i = 0; i < n; i++) {
+        //Conflicto en fila
+        for (let j = 0; j < n; j++) {
+            if (columnaEnConflicto != j) {
+                if (i == reinas[j]) {
+                    conflictos[i]++
+                }
+            }
+        }
 
 
-        if (reinas_en_conflicto.length == 0) {
-            console.log("Encontre la solución")
+        //Conflicto en diagonal derecha
+        for (let j = 0; j < n; j++) {
+            if (columnaEnConflicto != j) {
+                if (columnaEnConflicto + i == reinas[j] + j) {
+                    conflictos[i]++
+                    
+                }
+            }
+        }
+
+        //Conflicto en diagonal izquierda
+        for (let j = 0; j < n; j++) {
+            if (columnaEnConflicto != j) {
+                if (columnaEnConflicto - i == j - reinas[j]) {
+                    conflictos[i]++
+                }
+            }
+        }
+
+    }
+    return conflictos
+}
+function getNuevaPosicion(conflictos){
+    
+    //Encontrar minimo de conflictos
+    let n = conflictos.length
+    let minimo = Infinity
+    for (let i = 0; i < n; i++) {
+        if (minimo > conflictos[i]) {
+            minimo = conflictos[i]
+        }
+    }
+
+    let minimos = new Array()
+    for (let i = 0; i < n; i++) {
+        if (minimo == conflictos[i]) {
+            minimos.push(i)
+        }
+    }
+    
+    return minimos[getRandomInt(minimos.length)]
+    
+}
+const algoritmo_heuristico = (graficar_reina,n) => {
+    n = parseInt(n)
+    let reinas = new Array(n)
+    
+    inicializarTableroAleatoriamente(reinas)
+    let paso = 0
+    let ultimaColumna = -1
+    while (true) {
+        paso++
+        let reinasEnConflicto = getVariablesEnConflicto(reinas)
+        if (reinasEnConflicto.length == 0) {
+            console.log(reinas) 
+            console.log(paso)
             for (let i = 0; i < n; i++) {
                 graficar_reina(reinas[i],i,ACTIONS.COLOCAR)
             }
+            //return {reinas: reinas, pasos: paso}
             break;
         }
-        mostrar(reinas)
-        columna_en_conflicto = reinas_en_conflicto[getRandomInt(reinas_en_conflicto.length)]
-        while (columna_en_conflicto == ultima_columna)
-            columna_en_conflicto = reinas_en_conflicto[getRandomInt(reinas_en_conflicto.length)]
-        ultima_columna = columna_en_conflicto
-        console.log("Esta es la columna conflictiva: " + columna_en_conflicto)
-
-        //Encontrar numero de conflictos
-        let conflictos = new Array(n)
-        for(let i=0;i<n;i++){
-            conflictos[i]=0
-        }
-        console.log(conflictos)
-        //La i representa la fila
-        for (let i = 0; i < n; i++) {
-            //Conflicto en fila
-            for (let j = 0; j < n; j++) {
-                if (columna_en_conflicto != j) {
-                    if (i == reinas[j]) {
-                        conflictos[i]++
-                    }
-                }
-            }
-
-
-            //Conflicto en diagonal derecha
-            for (let j = 0; j < n; j++) {
-                if (columna_en_conflicto != j) {
-                    if (columna_en_conflicto + i == reinas[j] + j) {
-                        conflictos[i]++
-                        
-                    }
-                }
-            }
-
-            //Conflicto en diagonal izquierda
-            for (let j = 0; j < n; j++) {
-                if (columna_en_conflicto != j) {
-                    if (columna_en_conflicto - i == j - reinas[j]) {
-                        conflictos[i]++
-                    }
-                }
-            }
-
-        }
-        console.log("Estos son los conflictos")
-        console.log(conflictos)
-        //Encontrar minimo de conflictos
-        let minimo = Infinity
-        for (let i = 0; i < n; i++) {
-            if (minimo > conflictos[i]) {
-                minimo = conflictos[i]
-            }
-        }
-
-        let minimos = new Array()
-        for (let i = 0; i < n; i++) {
-            if (minimo == conflictos[i]) {
-                minimos.push(i)
-            }
-        }
-        
-        let nueva_posicion = minimos[getRandomInt(minimos.length)]
-        console.log("la nueva posicion es: " + nueva_posicion )
-        
-        
-        reinas[columna_en_conflicto] = nueva_posicion
-        
-
-
+        let columnaEnConflicto = getColumnaEnConflicto(reinasEnConflicto,ultimaColumna)
+        ultimaColumna = columnaEnConflicto
+        let conflictos = getConflictosEnColumna(reinas,columnaEnConflicto)
+        let nueva_posicion = getNuevaPosicion(conflictos)
+        reinas[columnaEnConflicto] = nueva_posicion
     }
-    //Fin del algoritmo
-    
-
 };
