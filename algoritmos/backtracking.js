7ff42b595bde8b0c2ff90b58bfa2cd10961029d8
Franfercs <ffcs98@hotmail.com>
2021-08-15 17:12:20 -0400

    Retorna todos los resultados

--- algoritmos/backtracking.js
+++ algoritmos/backtracking.js
@@ -12,6 +12,20 @@ function verificar_conflictos(columnas) {
 
     return false
 }
+
+var print_board = function (columns) {
+    var n = columns.length, row = 0, col = 0
+    while (row < n) {
+      while (col < n) {
+        process.stdout.write(columns[row] === col ? 'Q ' : '# ')
+        col++
+      }
+  
+      process.stdout.write('\n')
+      col = 0
+      row++
+    }
+}
   
 function colocar_reina(total, reinas, columnas) {
     if (reinas === 0) return columnas
@@ -29,10 +43,32 @@ function colocar_reina(total, reinas, columnas) {
     }
     return undefined
 }
+
+const soluciones = []
+function colocar_reina2(total, reinas, columnas) {
+
+    if (reinas === 0){
+        for(var i = 0; i < total; i++){
+            soluciones.push(columnas[i])
+        }
+        return columnas
+    }
+    columnas = columnas || []
+    for (var columna = 0; columna < total; columna++) {
+        columnas.push(columna)
+        estados.add(columnas.join(''))
+        pasos_backtracking++
+        if (!verificar_conflictos(columnas) &&
+            colocar_reina2(total, reinas - 1, columnas)) {
+        }
+        columnas.pop(columna)
+    }
+}
+
 const algoritmo_backtracking = (n) => {
 
     n = parseInt(n)
     reinas = colocar_reina(n, n)
     return reinas ? {reinas:reinas, pasos: pasos_backtracking,estados:estados.size} : undefined;
 }
-  
+
